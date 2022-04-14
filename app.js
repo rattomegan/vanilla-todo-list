3fcfc99948686d7c74ead5f40c3ab4998c514f51
Megan Ratto <megratt@gmail.com>
2022-04-14 13:54:12 -0400

    Add live time

--- app.js
+++ app.js
@@ -135,3 +135,18 @@ function checkLocalStorage() {
   // if our todo array doesn't exist, create on, otherwise, parse 
   return !localStorage.getItem('todos') ? todos = [] : todos = JSON.parse(localStorage.getItem('todos'));
 }
+
+
+
+// Live Time
+
+function refreshTime() {
+  const timeDisplay = document.getElementById("time");
+  const dateString = new Date().toLocaleString();
+  const formattedString = dateString.replace(", ", " ");
+  timeDisplay.textContent = formattedString;
+}
+
+refreshTime();
+
+setInterval(refreshTime, 1000);
